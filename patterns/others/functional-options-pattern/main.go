a391e15e891e1411fb3bbb5f4eca500e5adfacba
Edgard Barquero <ebarquero85@gmail.com>
2023-06-30 20:08:29 -0600

    Rewrite funcitonal options patters

--- patterns/others/functional-options-pattern/main.go
+++ patterns/others/functional-options-pattern/main.go
@@ -1,64 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type Opts struct {
-	maxConn int
-	id      string
-	tls     bool
+type OptFunc func(*Options)
+
+type Options struct {
+	host string
+	id   int
+	tls  bool
 }
 
 type Server struct {
-	Opts
+	options Options
 }
 
-type OpFunc func(*Opts)
-
-// =======
-
-func defaultOpts() Opts {
-	return Opts{
-		maxConn: 10,
-		id:      "one",
-		tls:     false,
+func defaultOptions() Options {
+	return Options{
+		host: "127.0.0.1",
+		id:   10,
+		tls:  false,
 	}
 }
 
-func withTLS(opts *Opts) {
-	opts.tls = true
-}
+func NewServer(opts ...OptFunc) *Server {
 
-func withMaxConn(mc int) OpFunc {
-	return func(opts *Opts) {
-		opts.maxConn = mc
-	}
-}
-
-func withID(id string) OpFunc {
-	return func(opts *Opts) {
-		opts.id = id
-	}
-}
-
-// =======
-
-// the slice of OpFunc (...OpFunc) give us the posibility of send 0, 1, 2 etc options
-func newServer(opts ...OpFunc) *Server {
-
-	o := defaultOpts()
+	o := defaultOptions()
 
 	for _, fn := range opts {
 		fn(&o)
 	}
 
 	return &Server{
-		Opts: o,
+		options: o,
+	}
+}
+
+// Options functions
+func withTLS(o *Options) {
+	o.tls = true
+}
+
+func withHost(host string) OptFunc {
+	return func(o *Options) {
+		o.host = host
+	}
+}
+
+func withId(id int) OptFunc {
+	return func(o *Options) {
+		o.id = id
 	}
 }
 
 func main() {
 
-	s := newServer(withTLS, withMaxConn(100), withID("two"))
-	fmt.Printf("%v", s)
+	//server := NewServer() // Default values
+
+	server := NewServer(
+		withTLS,
+		withHost("192.168.1.0"),
+		withId(99),
+	)
+
+	fmt.Printf("%+v\n", server)
 
 }
